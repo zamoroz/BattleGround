bddfdde3af236e197637b4ac2f18c123c49b36af
у <>
2021-03-13 14:40:35 +0700

    fighting rework

--- Fight.py
+++ Fight.py
@@ -22,26 +22,23 @@ class Fight:
     def fight(self):
         player = self.player
         enemy = self.enemy
-        print(player.name)
-        print("/HP/St/Pr")
-        print(str(player.health) + "/" + str(player.strenght) + "/" + str(player.protection))
-        print("-"*15)
-        print(enemy.name)
-        print("/HP/St/Pr")
-        print(str(enemy.health) + "/" + str(enemy.strenght) + "/" + str(enemy.protection))
-        print("-"*15)
 
         damage = randint(1, player.strenght) - randint(1, enemy.protection)
         if damage < 1: damage = 1
-        typing("Ты нанес " + str(damage) + " урона врагу.")
         enemy.add_health(-damage)
+        print("-"*30)
+        typing("Ты нанес " + str(damage) + " урона врагу.")
+        typing("Осталось " + str(enemy.health))
 
         damage = randint(1, enemy.strenght) - randint(1, player.protection)
         if damage < 1: damage = 1
-        typing(enemy.name + " нанес " + str(damage) + " урона тебе.")
         player.add_health(-damage)
+        print("-"*30)
+        typing(enemy.name + " нанес " + str(damage) + " урона тебе.")
+        typing("Отсалось " + str(player.health))
 
         if player.health <= 0:
+            print("-"*30)
             typing("Вы погибли.")
             input()
 
@@ -57,16 +54,17 @@ class Fight:
     def direction(self):
         player = self.player
         enemy = self.enemy
-        typing("Ты искал битвы и нашел себе врага.")
+        typing("На тебя напал монстр "+ enemy.name +".")
         typing("Характеристики врага:")
-        typing("Имя " + enemy.name)
         typing("Сила " + str(enemy.strenght))
         typing("Точность " + str(enemy.accuaracy))
         typing("Защита " + str(enemy.protection))
+        typing("Здоровье " + str(enemy.health))
+        print("-"*30)
         typing("Что будешь делать?")
         def solve_request():
-            typing("1)Сражаться")
-            typing("2)бежать")
+            print("1) Сражаться")
+            print("2) Бежать")
             solve = input("> ")
             if solve == "1":
                 typing("Начинается бой с " + enemy.name)
@@ -76,7 +74,8 @@ class Fight:
                     typing("Убежать не удалось, придется сражаться...")
                     self.fight()
                 else:
-                    typing("Ты убежал от этого врага, но кто знает, что ждеть тебя в следующую секунду.")
+                    typing("Ты убежал от монстра "+ enemy.name + ".")
+                    print("-"*30)
                     player.add_exp(5)
             else:
                 typing("Ты что-то напутал, давай попробуем еще раз.")
