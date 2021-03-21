3f62afa2db521344fbbd7cda09dbc904b4072a02
owl <moras8465132@gmail.com>
2021-03-21 13:04:47 +0700

    add max_health

--- Player.py
+++ Player.py
@@ -2,24 +2,16 @@ from typing import typing
 
 class Player:
 
-    strenght: int #сила
-    dexterity: int #ловкость
-    protection: int #защита
-    helth: int #здоровье
-    name: str #имя
-    exp: int
-    lvl: int
-
     def __init__(self):
         self.strenght = 5
         self.dexterity = 5
         self.protection = 5
-        self.health = 100
+        self.max_health = 100
+        self.health = self.max_health
         self.name = ""
         self.exp = 0
         self.lvl = 0
-        self.inventory = []
-    
+
     def add_strenght(self, i):
         self.strenght += i
     
@@ -31,6 +23,8 @@ class Player:
 
     def add_health(self, i):
         self.health += i
+        if self.health > self.max_health:
+            self.health = self.max_health
     
     def set_name(self, name):
         self.name = name
@@ -38,17 +32,16 @@ class Player:
     def add_exp(self, i):
         self.exp += i
     
+    def add_max_health(self, i):
+        self.max_health += i
+    
     def levelUp(self):
         typing("Поздравляю! вы повысили уровень, все характеристики выросли.")
         self.lvl += 1
         self.exp = 0
         self.add_dexterity(1 + int(0.5 * self.lvl))
-        self.add_health(30)
+        self.add_max_health(20)
+        self.health = self.max_health
         self.add_protection(1 + int(0.5 * self.lvl))
         self.add_strenght(1 + int(0.5 * self.lvl))
     
-    def add_item(self, item):
-        self.inventory.append(item)
-    
-    def remove_item(self, item):
-        pass
